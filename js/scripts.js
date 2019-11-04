5d70b6bc2b938a6b619decfaf677b7b6886cce39
kraft-owaka <owakakraft@gmail.com>
2019-11-04 15:49:31 +0300

    added the jquery code and the css

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@ $(document).ready(function() {
         $(".design-showing").toggle();
     });
   });
+  $(document).ready(function() {
+    $(".clickable2").click(function() {
+        $(".development-hidden").toggle();
+        $(".development-showing").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".clickable2").click(function() {
+        $(".product-hidden").toggle();
+        $(".product-showing").toggle();
+    });
+  });
